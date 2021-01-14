12d24ce34bae045cbae018a29246884ace39746d
Floyd Hightower <floyd.hightower27@gmail.com>
2021-01-14 10:27:44 -0500

    Importing atomic_write function

--- democritus_file_system/__init__.py
+++ democritus_file_system/__init__.py
@@ -2,5 +2,6 @@ __author__ = '''Floyd Hightower'''
 __email__ = 'floyd.hightower27@gmail.com'
 __version__ = '2021.01.12'
 
+from .atomic_writes import atomic_write
 from .files import *
 from .directories import *
